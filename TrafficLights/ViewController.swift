47715137accfa8b22b8d0c3a79e829ee7fa73b1c
Vladimir Kiselev <first3395@gmail.com>
2022-03-15 23:22:39 +0300

    Added readme, layout and some logic. TODO: switch lights on button press

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var switchLightsButton: UIButton!
+    
+    enum LightsState {
+        case off
+        case red
+        case yellow
+        case green
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 64
+        yellowLightView.layer.cornerRadius = 64
+        greenLightView.layer.cornerRadius = 64
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
     }
 
-
+    @IBAction func switchingLightsButton() {
+        // On start -> red on
+        redLightView.alpha = 1.0
+        // switch case -> through all views and make next alpha = 1, prev alpha = 0.3
+        
+        // if id = 0 -> Start if others -> Next
+        switchLightsButton.setTitle("Next", for: .normal)
+    }
+    
 }
 
