811fef71bd31255cf1555d414aaf88eb1224a0a9
Vladimir Kiselev <first3395@gmail.com>
2022-03-15 23:50:17 +0300

    Task done but button height is different

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,37 +7,56 @@
 
 import UIKit
 
+enum LightsState {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
+    
     @IBOutlet var switchLightsButton: UIButton!
     
-    enum LightsState {
-        case off
-        case red
-        case yellow
-        case green
-    }
+    private var currentLight = LightsState.red
+    private let isOn: CGFloat = 1.0
+    private let isOff: CGFloat = 0.3
+    private let lightsCornerRadius: CGFloat = 64
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redLightView.layer.cornerRadius = 64
-        yellowLightView.layer.cornerRadius = 64
-        greenLightView.layer.cornerRadius = 64
         
-        redLightView.alpha = 0.3
-        yellowLightView.alpha = 0.3
-        greenLightView.alpha = 0.3
+        switchLightsButton.layer.cornerRadius = 16
+        
+        redLightView.layer.cornerRadius = lightsCornerRadius
+        yellowLightView.layer.cornerRadius = lightsCornerRadius
+        greenLightView.layer.cornerRadius = lightsCornerRadius
+        
+        redLightView.alpha = isOff
+        yellowLightView.alpha = isOff
+        greenLightView.alpha = isOff
     }
 
     @IBAction func switchingLightsButton() {
-        // On start -> red on
-        redLightView.alpha = 1.0
-        // switch case -> through all views and make next alpha = 1, prev alpha = 0.3
-        
-        // if id = 0 -> Start if others -> Next
+
+        switch currentLight {
+        case .red:
+            greenLightView.alpha = isOff
+            redLightView.alpha = isOn
+            currentLight = .yellow
+        case .yellow:
+            redLightView.alpha = isOff
+            yellowLightView.alpha = isOn
+            currentLight = .green
+        case .green:
+            yellowLightView.alpha = isOff
+            greenLightView.alpha = isOn
+
+            currentLight = .red
+        }
+
         switchLightsButton.setTitle("Next", for: .normal)
     }
     
